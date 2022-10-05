c8bfb9af0d3fa8b872d4a3126103a0c4ac843537
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-10-05 10:59:32 +0100

    image array display

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import './App.css';
-import React from 'react';
-//import Board from './components/Board';
+import React, { useState } from 'react';
+
+const cardImages = [
+  {"src" : "/img/helmet-1.png"},
+  {"src" : "/img/potion-1.png"},
+  {"src" : "/img/ring-1.png"},
+  {"src" : "/img/scroll-1.png"},
+  {"src" : "/img/shield-1.png"},
+  {"src" : "/img/sword-1.png"}
+]
 
 
 
-const App = () => {
+function App(){
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] =  useState(0)
+
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random()}))
+
+  setCards(shuffleCards)
+  setTurns(0)
+  }
+
+console.log(cards, turns)
   return (
     <>
       <div className='app'>
         <h2>Magic Match</h2>
-        <button>New Game</button>
+        <button onClick={shuffleCards}>New Game</button>
+
+        <div className='card-grid'>
+            {cards.map(card => (
+              <div className='card' key={card.id}>
+                  <div>
+                    <img className='font' src={card.src} alt="card front"/>
+                    <img className='cover' src="/img/cover.png" alt='cover'/>
+                  </div>
+              </div>
+            ))}
+        </div>
       </div>
-         {/* <div className='boardlocation'>
-            <Board />
-         </div> */}
     </>
   );
-}
+} 
 
 export default App;
